dd13824859f53ee25c965e49f92cf6cda384c41d
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2023-04-06 23:09:37 +0400

    update

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 let burger = document.querySelector(".burger");
 let nav = document.querySelector("nav");
-burger.addEventListener("click", () => {
+burger.addEventListener("click", (e) => {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
   console.log([...burger.classList].includes("active"));
   if ([...burger.classList].includes("active")) {
-    document.querySelector(".bg").style.filter = "blur(5px)"
+    [...document.querySelectorAll(".bg")].map((a) => {
+      a.style.filter = "blur(5px)";
+      document.body.style.overflow="hidden"
+    });
     // document.querySelector("header").style.filter = "blur(5px)";
-  }else{
-    document.querySelector(".bg").style.filter = "blur(0px)"
+  } else {
+    [...document.querySelectorAll(".bg")].map((a) => {
+      a.style.filter = "blur(0px)";
+      document.body.style.overflow="auto"
+
+    });
   }
 });
